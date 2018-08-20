a7ff7ed2a589043e9db24b56e33b0ef0205d3d20
mat2 <mat1507@bluewin.ch>
2018-08-20 11:06:52 +0200

    v9

--- goog_drive_sync.py
+++ goog_drive_sync.py
@@ -32,6 +32,7 @@ import googleapiclient
 mime = MimeTypes()
 
 import time
+from functools import wraps
 import ssl
 import subprocess
 import hashlib
@@ -45,6 +46,15 @@ def usage():
     print(__doc__)
 
 
+def timing(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        t0 = time.time()
+        result = f(*args, **kwargs)
+        print (f.__name__,'dt / sec:',time.time()-t0)
+        return result
+    return wrapper
+
 def md5sum(self, filename, bs=65536):
 #        with open(filename, mode='rb') as f:
 #            d = hashlib.md5()
@@ -57,7 +67,7 @@ def md5sum(self, filename, bs=65536):
     return md5chksum
 
 
-def load_fl(self, fname='filelist_dump_raw.pickle'):
+def load_fl(fname='filelist_dump_raw.pickle'):
     return pickle.load(open(fname,'rb'))
 
 
@@ -66,17 +76,16 @@ class GD:
     def __init__(self, credential_filename = 'credentials2.json'):
         self.folder_mimetype = 'application/vnd.google-apps.folder'
         cred_filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), credential_filename)
-        SCOPES='https://www.googleapis.com/auth/drive' # all drive write scope
+        print(cred_filename)
+        SCOPES='https://www.googleapis.com/auth/drive'
         store = file.Storage(cred_filename)
         creds = store.get()
 
-        # rerun this anyway if change scape (now, we can do whatever write on the whole drive)
         if (not creds) or creds.invalid or (list(creds.scopes)[0] != SCOPES):
             flow = client.flow_from_clientsecrets('client_secret_google_drive.json', SCOPES)
             creds = tools.run_flow(flow, store)
         self.service = build('drive', 'v3', http=creds.authorize(Http()))
-        self.root_id = self.ls()['parents'][0][0]
-
+        self.root_id = self.ls()[0]['parents'][0]
 
     def walk_diff(self, gen_walk_os, gen_walk_drive):
         """ Return only what's not in drive. Skip when local fn and dn empty """
@@ -99,28 +108,29 @@ class GD:
     def walk(self, top_dir):
         """ similar to os.walk: for dp,dn,fn in gd.walk(folder_name):print(dp,dn,fn) """
         def lsw(dp):
-            l = self.ls(self.get_id_by_path(dp))
-            dn, fn = [], []
-            if l:
-                for i,el in enumerate(l['mimeType']):
-                    if el == self.folder_mimetype:
-                        dn.append(l['name'][i])
-                    else:
-                        fn.append(l['name'][i])
-                return dp,dn,fn
+            dp = dp.strip(os.path.sep)
+            l = self.list_files(""" "%s" in parents""" % self.get_id_by_path(dp), fields='name, mimeType')
+            dn = sorted([e['name'] for e in l if e['mimeType'] == self.folder_mimetype])
+            fn = sorted([e['name'] for e in l if e['mimeType'] != self.folder_mimetype])
+            return dp,dn,fn
+        try:
+            dp, dn, fn = lsw(top_dir)
+        except googleapiclient.errors.HttpError as e:
+            if e.resp.status == 404:
+                return
             else:
-                return dp,[],[]
-        dp, dn, fn = lsw(top_dir)
-        dn.sort()
-        yield (dp, dn, sorted(fn))
+                raise
+        yield (dp, dn, fn)
         for dir in dn:
-            yield from self.walk(dp+'/'+dir)
+            yield from self.walk( os.path.join(dp, dir) )
 
-
+    @timing
     def sync_from_list(self, g):
-        for dp, dn, fn in g:
-            print('syncing',dp)
+        g = [e for e in g]
+        lth = len(g); print('syncing length',lth)
+        for i, (dp, dn, fn) in enumerate(g):
             parent_id = self.get_id_by_path(dp)
+            print('syncing','%i/%i' %(i,lth),'to',parent_id,'(',dp,')')
             if parent_id is None:
                 parent_id = self.create_folder_by_path(dp)
             for d in dn:
@@ -130,21 +140,16 @@ class GD:
             print()
 
 
-    def list_files(self, q, fields):
+    def list_files(self, q, fields = 'id, name, mimeType'):
         """ use for instance :
             q = "'parent_id' in parents", "name contains 'xxxyyy'",
                 "mimeType='%s'" % self.folder_mimetype
             fields = 'id, name, parents, mimeType, md5Checksum'
         """
         page_token = None
+        itoken = 0
         filelist = []
-
-        if q:
-            q += ' and trashed=false'
-        else:
-            q = 'trashed=false'
-
-        print('list files q =', q, ', fields =', fields)
+        q = ' and '.join(filter(None, ["trashed=false and 'me' in owners",q.strip()]))
         while True:
             for _ in range(num_retries):
                 try:
@@ -152,13 +157,18 @@ class GD:
                                               fields='nextPageToken, files(%s)' % fields,
                                               pageToken=page_token).execute()
                     break
-                except Exception:
+                except googleapiclient.errors.HttpError as e:
+                    if e.resp.status == 404:
+                        print('not found')
+                        raise
                     time.sleep(5)
             else:
                 print('ERROR list_files',q,fields)
                 raise
-            
             filelist += response.get('files', [])
+            print('.',end='',flush=True)
+            itoken += 1
+            if itoken % 50 == 0: print(len(filelist),end='',flush=True)
             page_token = response.get('nextPageToken', None)
             if page_token is None:
                 break
@@ -170,81 +180,71 @@ class GD:
 
     def file_exists(self, id):
         try:
-            if self.service.files().get(fileId=id, fields='trashed').execute()['trashed']:
+            d = self.service.files().get(fileId=id, fields='trashed,shared').execute()
+            if d['trashed'] or d['shared']:
                 return False
         except googleapiclient.errors.HttpError as e:
-            if "File not found" in str(e):
+            if e.resp.status == 404:
+                print('not found')
                 return False
             else:
-                print('file_exists: unknown error',e)
+                print('file_exists: unknown error, status:',e.resp.status)
         return True
 
 
     def ls(self, parent_id='root', filename = None): # should allow wildcard for filename
         if parent_id is None:
             return None
-
         if filename is None:
             q=""" "%s" in parents""" % parent_id
         elif filename.endswith('*'): # google drive api "contains" is actually "startswith"
             q=""" "%s" in parents and name contains "%s" """ % (parent_id, filename[:-1])
         else:
             q=""" "%s" in parents and name="%s" """ % (parent_id, filename)
-
         filelist = self.list_files(q,fields='id, name, parents, md5Checksum, mimeType')
-        for i,file in enumerate(filelist):
-            if 'md5Checksum' not in file:
-                 filelist[i]['md5Checksum'] = None
-        return {k: [d[k] for d in filelist] for k in filelist[0].keys()} if filelist else {}
-
+        for el in filelist:
+            if 'md5Checksum' not in el:
+                 el['md5Checksum'] = None
+        return filelist
 
     def ls_by_path(self, path, filename = None):
-        return self.ls( self.get_id_by_path(path.rstrip('/')), filename )
+        return self.ls( self.get_id_by_path( path.strip(os.path.sep) ), filename )
 
     def get_id(self, filename, parent_id='root'):
-        filedict = self.ls(parent_id,filename)
-        return filedict['id'][0] if filedict else None
+        filedict = self.ls(parent_id, filename)
+        return filedict[0]['id'] if filedict else None
         
     def get_id_by_path(self, path):
-        l = path.split('/')
+        l = path.strip(os.path.sep).split(os.path.sep)
         for i,filename in enumerate(l):
             if i == 0:
-                id = self.get_id(filename,parent_id='root')
+                id = self.get_id(filename, parent_id = 'root')
             else:
-                id = self.get_id(filename,id)
+                id = self.get_id(filename, id)
             if id is None:
                 return
         return id
 
 
-
     def create_folder_by_path(self, folder_name):
         """ create folder in drive, e.g 'a/b/c' will create a in root, b in a and c in b """
-        folder_list = folder_name.split('/')
+        folder_list = folder_name.split(os.path.sep)
         for i,folder in enumerate(folder_list):
             if i == 0:
-                id = self.create_folder(folder,parent_id='root')
+                id = self.create_folder(folder, parent_id = 'root')
             else:
-                id = self.create_folder(folder,parent_id=id)
+                id = self.create_folder(folder, parent_id = id)
         return id
-
 
     def create_folder(self, folder_name, parent_id = 'root'):
         if not self.file_exists(parent_id):
-             print('parent folder does not exists',parent_id)
+             print('parent folder does not exists', parent_id)
              return
-
         id = self.get_id(folder_name, parent_id)
         if id is not None:
             # print('folder',folder_name,'already exists with id',id)
             return id
-
-        file_metadata = {
-            'name': folder_name,
-            'mimeType': 'application/vnd.google-apps.folder',
-            'parents': [parent_id]
-        }
-
+        file_metadata = {'name': folder_name, 'mimeType': self.folder_mimetype, 'parents': [parent_id]}
         for _ in range(num_retries):
             try:
                 id = self.service.files().create(body=file_metadata, fields='id').execute()['id']
@@ -254,23 +254,20 @@ class GD:
         else:
             print('ERROR create_folder',file_metadata)
             raise
-
         print('folder', folder_name, 'created with id',id,'in parent folder',file_metadata['parents'])
         return id
-
 
 
     def upload(self, filename_with_path, parent_id, md5_check=True):
         filename = os.path.basename(filename_with_path)
         filedict = self.ls(parent_id, filename)
         if filedict:
-            ids = filedict['id']
+            ids = [el['id'] for el in filedict]
             print('file',filename_with_path,'already uploaded with ids',ids,'. Checking ckecksum')
             md5_local = md5sum(filename_with_path)
-            md5_drive = filedict['md5Checksum']
+            md5_drive = [el['md5Checksum'] for el in filedict]
 
             if md5_local in md5_drive:
-                # print( md5_drive.index(md5_local), filedict['md5Checksum'])
                 return ids[ md5_drive.index(md5_local) ]
             else:
                 print('delete current and upload new one not implemented. Upload new instance of file',filename)
@@ -288,8 +285,7 @@ class GD:
         else:
             print('ERROR upload',body)
             raise
-
-        print('uploaded', filename_with_path, mime_type, 'with id', id)
+        print('uploaded',id,'to',parent_id,':', filename)
         return id
 
 
@@ -297,31 +293,26 @@ class GD:
         filename = os.path.basename(filename_with_path)
         dirname =  os.path.dirname(filename_with_path)
         parent_id = self.get_id_by_path(dirname) if dirname else 'root'
-        id = self.upload(filename_with_path,parent_id)
+        id = self.upload(filename_with_path, parent_id)
         return id
 
-
+    @timing
     def ls_drive(self, only_dir = False, dump = True):
         """
         recursive ls from basedir: takes about 10 minute for 200'000 files in the drive
         (retrieve 1000 entries every 3 seconds)
         """
-        t0 = time.time()
         fl = self.list_files(q = "mimeType='%s'" % self.folder_mimetype if only_dir else '',
                                    fields = 'id, name, parents, mimeType')
-        fl = [e for e in fl if len(e.keys())==4] # list of dict
+        fl = [e for e in fl if len(e.keys())==4]
 
         if dump:
-            fff = open('filelist_dump_raw.pickle','wb')
-            pickle.dump(fl,fff)
-            fff.close()
-
+            pickle.dump(fl, open('filelist_dump_raw.pickle','wb'))
         return fl
 
 
-
     def get_tree(self, topdir, fl):
-        """ build dict-like directory hirarchy tree from a given root """
+        """ build dict-like directory hirarchy tree from a given root. """
         w = {e['id']: e for e in fl if len(e.keys())==4}
         w[self.root_id] = {'id': self.root_id, 'parents': [None], 'name': '', 'mimeType': self.folder_mimetype}
         for id in w:
@@ -350,44 +341,42 @@ class GD:
 
 
     def walk_from_treedict(self, d, max_depth=-1):
+        """ walk by recursing the tree dictionary """
         if not d:
             return
 
-        fn,dn=[],[]
-
+        fn, dn = [], []
         for el in d['content']:
-            el['tree'] = (d['tree']+'/'+el['name']).strip('/')
+            el['tree'] = ( os.path.join( d['tree'], el['name']) ).strip( os.path.sep )
             if el['is_folder']:
                 dn.append(el['name'])
             else:
                 fn.append(el['name'])
-
         yield (d['tree'],sorted(dn),sorted(fn))
 
         if max_depth==0:
             return
+
         for el in d['content']:
             if el['is_folder']:
                 yield from self.walk_from_treedict(d = el, max_depth = max_depth - 1)
 
 
     def walk_from_ls_drive(self, top_dir, max_depth=-1):
-        #fl = load_fl()
-        fl = self.ls_drive()
+        fl = load_fl()
+        #fl = self.ls_drive()
         d = self.get_tree(top_dir,fl)
         yield from self.walk_from_treedict(d, max_depth)
     
-
 #    def print_treedict(self, top_dir, max_depth=-1):
 #        g = self.walk_from_ls_drive(top_dir, max_depth)
 #        for el in g: print(el)
 
-
+    @timing
     def sync_naive(self, top_dir):
         """ sync local not in the drive (basic one way sync).
             Walk through local and sync missing drive files/dir
         """
-        t0 = time.time()
         for dp, dn, filenames in os.walk(top_dir):
             print('syncing',dp)
             parent_id = self.get_id_by_path(dp)
@@ -397,41 +386,22 @@ class GD:
                 filename = os.path.join(dp, f)
                 self.upload(filename,parent_id)
             print()
-        t1 = time.time()
-        print('walk diff dt',t1-t0)
 
-
-    def sync(self, top_dir):
-        """ generate differences of local and drive walks. Then sync over this result
-        """
-        t0 = time.time()
-        g = self.walk_diff(os.walk(top_dir), self.walk(top_dir))
-        self.sync_from_list(g)
-        t1 = time.time()
-        print('walk diff dt',t1-t0)
-
-
-    def sync_from_ls_drive(self,top_dir):
-        """
-            Sync with first retrieving full file list and the compute differences.
+    @timing
+    def sync(self, top_dir, from_treedict = False):
+        """ generate differences of local and drive walks. Then sync over the result.
+            if from_treedict: the full file list is first retrieved, a tree is generated
+            and then the drive walk is built.
             No md5 check
         """
-        t0 = time.time()
-        g = self.walk_diff(os.walk(top_dir), self.walk_from_ls_drive(top_dir))
-        g = [e for e in g] # convert gen to list to measure time
-        t1 = time.time()
-        print('walk diff dt',t1-t0)
-        
+        walk = self.walk_from_ls_drive(top_dir) if from_treedict else self.walk(top_dir)
+        g = self.walk_diff(os.walk(top_dir), walk)
         self.sync_from_list(g)
-        t2 = time.time()
-        print('sync dt',t2-t1)
-        return g
-
 
 
 # from goog_drive_sync_v8 import *;gd=GD()
 # top_dir='psitest'
-# gd.sync(top_dir) # or sync_naive or sync_from_ls_drive
+# gd.sync(top_dir) # or sync_naive, option: from_treedict=False
 
 
 
